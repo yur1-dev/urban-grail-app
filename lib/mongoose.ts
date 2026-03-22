533faa6f79646dc837d14dda04986539824fba5e
yur1-dev <yuriesb01@gmail.com>
2026-03-22 16:06:09 +0800

    up

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,19 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in .env.local");
-}
-
-let cached = (global as any).mongoose || { conn: null, promise: null };
-(global as any).mongoose = cached;
-
 export async function connectDB() {
-  if (cached.conn) return cached.conn;
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  if (mongoose.connection.readyState >= 1) return;
+
+  await mongoose.connect(process.env.MONGODB_URI!, {
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 10000,
+  });
 }
